32922009d9ec498ea03a882b28dac97b070fbcf7
Tomás Martins <tomgodmartins@gmail.com>
2026-07-17 03:52:33 +0100

    unfinished

--- Content.Client/Clothing/ClientClothingSystem.cs
+++ Content.Client/Clothing/ClientClothingSystem.cs
@@ -39,6 +39,7 @@ public sealed partial class ClientClothingSystem : ClothingSystem
         {"back", "BACKPACK"},
         {"belt", "BELT"},
         {"gloves", "HAND"},
+        {"pants", "LEGS"}, // Oathlord
         {"shoes", "FEET"},
         {"id", "IDCARD"},
         {"pocket1", "POCKET1"},
